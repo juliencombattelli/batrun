3de58ecc9c4ca383d85d2eae9d89633495de8ea4
Julien Combattelli <julien.combattelli@gmail.com>
2025-06-14 18:35:18 +0200

    Remove not needed async in sequential executor

--- src/test_executor/sequential.rs
+++ src/test_executor/sequential.rs
@@ -1,6 +1,5 @@
 use crate::error::Error;
 use crate::test_driver::TestDriver;
-use crate::test_executor::utils::simple_executor;
 use crate::test_executor::{ExecutionContext, Executor};
 use crate::test_suite::visitor::Visitor;
 
@@ -13,24 +12,18 @@ impl Executor for SequentialExecutor {
         test_driver: &Box<(dyn TestDriver + 'static)>,
     ) {
         for exec_context in execution_contexts {
-            let task = async || {
-                let target = exec_context.target.clone();
-                let test_suite_dir = exec_context.test_suite.path();
-                let mut visitor = Visitor::new(&exec_context.test_suite);
-                loop {
-                    let (done, _) =
-                        visitor.visit_next(|test_case, should_skip| -> Result<(), Error> {
-                            let _tc_state =
-                                test_driver.run_test(test_suite_dir, &target, test_case)?;
-                            Ok(())
-                        });
-                    if done {
-                        break;
-                    }
-                    simple_executor::wait_until_next_poll().await;
+            let target = exec_context.target.clone();
+            let test_suite_dir = exec_context.test_suite.path();
+            let mut visitor = Visitor::new(&exec_context.test_suite);
+            loop {
+                let (done, _) = visitor.visit_next(|test_case, should_skip| -> Result<(), Error> {
+                    let _tc_state = test_driver.run_test(test_suite_dir, &target, test_case)?;
+                    Ok(())
+                });
+                if done {
+                    break;
                 }
-            };
-            simple_executor::block_on(task());
+            }
         }
     }
 }
