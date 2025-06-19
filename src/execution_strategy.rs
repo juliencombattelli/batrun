aca55e231a8dd910b092118747ed717d7f0d004b
Julien Combattelli <julien.combattelli@gmail.com>
2025-06-19 08:15:50 +0200

    Add strategy aliases to CLI

--- src/execution_strategy.rs
+++ src/execution_strategy.rs
@@ -1,10 +1,15 @@
+/// Aliases are currently not showed in help message.
+/// Wait for https://github.com/clap-rs/clap/pull/5480 to be merged to make aliases visible in help message.
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(feature = "clap", derive(clap::ValueEnum))]
 pub enum ExecutionStrategy {
     /// Run all test cases sequentially for a target before passing to the next target
+    #[clap(aliases = &["s", "seq"])]
     Sequential,
     /// Run each test case for all targets before passing to the next test case
+    #[clap(aliases = &["r", "rr"])]
     RoundRobin,
     /// Run all test cases for each targets in parallel
+    #[clap(aliases = &["p", "par"])]
     Parallel,
 }
