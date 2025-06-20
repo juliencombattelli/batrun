da60861d2c535b66895ceec74ac0ac4b03921906
Julien Combattelli <julien.combattelli@gmail.com>
2025-06-20 18:22:50 +0200

    Add ivts test cases with no setup/teardown

---
+++ tests/ivts/01-ivts/07-no-teardown.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function setup {
+    return 0
+}
+
+function test_01_ok {
+    declare -p KNOWN_DEVICES_IP
+    local -r DEVICE="$1"
+    local -r OUT_DIR="$2"
+    local -r DEVICE_IP="$(resolve_device_ip DEVICE)"
+    echo "Test 01 on device $DEVICE_IP" > "$OUT_DIR/test1"
+    return 0
+}
+
+function test_02_fail {
+    local -r DEVICE="$1"
+    local -r OUT_DIR="$2"
+    local -r DEVICE_IP="$(resolve_device_ip DEVICE)"
+    echo "Test 02 on device ${KNOWN_DEVICES_IP[$DEVICE]}" > "$OUT_DIR/test2"
+    return 1
+}
