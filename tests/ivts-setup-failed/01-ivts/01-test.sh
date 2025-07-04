1d5bba8d2ff359197fb666aed7329080562bd187
Julien Combattelli <julien.combattelli@gmail.com>
2025-07-04 17:51:46 +0200

    Fix errors in ivts-setup-failed

--- tests/ivts-setup-failed/01-ivts/01-test.sh
+++ tests/ivts-setup-failed/01-ivts/01-test.sh
@@ -5,12 +5,10 @@ function setup {
 }
 
 function test_01_ok {
-    echo "Test 01 on device $DEVICE" > "$OUT_DIR/test1"
     return 0
 }
 
 function test_02_fail {
-    echo "Test 02 on device $DEVICE" > "$OUT_DIR/test2"
     return 1
 }
 
